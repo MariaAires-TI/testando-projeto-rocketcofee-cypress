d32a15a4fbc3c1f59ba8baae7a14c09087c30896
Maria Aparecida <cidinhalima_12@hotmail.com>
2023-01-05 11:23:06 -0300

    feat: test 2: checks if the order form opens in another tab without the need for a click

--- cypress/e2e/integration/coffe.cy.js
+++ cypress/e2e/integration/coffe.cy.js
@@ -2,7 +2,11 @@ describe('Central de Atendimento ao Cliente Great Coffe', function() {
     it('verifica o título da aplicação', function() {
         cy.visit('./src/index.html')
 
-        cy.title().should('be.equal', 'Great Coffe')
-  
-    })    
+        cy.title().should('be.equal', 'Great Coffe')         
+    })  
+    
+    it.only('verifica se o formulário de pedidos abre em outra aba sem a necessidade de um clique', function() {
+        cy.visit('./src/index.html')
+        cy.get('.buttonHeader').should('have.attr', 'target', '_blank')
+    });
   })
