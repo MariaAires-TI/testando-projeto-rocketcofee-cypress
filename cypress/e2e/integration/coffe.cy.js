b8514fc2d2d8f5867ece8d056b219895d16af0b4
Maria Aparecida <cidinhalima_12@hotmail.com>
2023-01-05 12:15:56 -0300

    refator: insert contexts in code

--- cypress/e2e/integration/coffe.cy.js
+++ cypress/e2e/integration/coffe.cy.js
@@ -1,27 +1,44 @@
 describe('Central de Atendimento ao Cliente Great Coffe', function() {
-    it('verifica o título da aplicação', function() {
-        cy.visit('./src/index.html')
+    context('testando a página inicial', function() {
 
-        cy.title().should('be.equal', 'Great Coffe')         
-    })  
+        beforeEach( () => {
+            cy.visit('./src/index.html')
+        })
+
+        it('verifica o título da aplicação', function() {               
+            cy.title().should('be.equal', 'Great Coffe')         
+        })  
+        
+        it('verifica se o formulário de pedidos abre em outra aba sem a necessidade de um clique', function() {
+            cy.get('.buttonHeader').should('have.attr', 'target', '_blank')        
+        });
+    });
+
+    context('testando a páginade formulário de pedidos', function() {
+        
+        beforeEach( () => {
+            cy.visit('./src/form.html')
+        })
+
+        it('visita formulário de pedidos', function() {
+            cy.visit('./src/form.html')
+        });
     
-    it('verifica se o formulário de pedidos abre em outra aba sem a necessidade de um clique', function() {
-        cy.visit('./src/index.html')
-        cy.get('.buttonHeader').should('have.attr', 'target', '_blank')        
+        it('verifica se ao clicar no botão home, o usuário é redirecionado para página inicial sem a necessidade de um clique', function() {
+            cy.get('.home').should('have.attr', 'target', '_blank')        
+        });  
+    
+        it('verifica o título da aplicação', function() {
+            cy.title().should('be.equal', 'Pegar meu café')
+        })
+    
+        // it('preenche os campos obrigatórios e envia', function() {
+        //     const longText = 'Adição de calda de chocolate'
+            
+        // });
     });
+    
 
-    it('visita formulário de pedidos', function() {
-        cy.visit('./src/form.html')
-    });
-
-    it('verifica se ao clicar no botão home, o usuário é redirecionado para página inicial sem a necessidade de um clique', function() {
-        cy.visit('./src/form.html')
-        cy.get('.home').should('have.attr', 'target', '_blank')        
-    });  
-
-    it.only('verifica o título da aplicação', function() {
-        cy.visit('./src/form.html')
-        cy.title().should('be.equal', 'Pegar meu café')
-    })
+    
 
 })
